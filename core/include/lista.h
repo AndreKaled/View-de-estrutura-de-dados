6c9d64df4ab8e9dafe7058312f520da4573780cf
AndreKaled <andrekaled9@gmail.com>
2025-12-19 17:46:05 -0400

    implementação parcial da lista

---
+++ core/include/lista.h
@@ -0,0 +1,128 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct ListaNo{
+    int valor;
+    struct ListaNo* prox;
+} ListaNo;
+
+typedef struct Lista{
+    ListaNo* inicio;
+    int tam;
+} Lista;
+
+/**
+ * Cria uma lista encadeada vazia.
+ *
+ * @return Ponteiro para Lista alocada dinamicamente,
+ *         ou NULL em caso de falha de alocação.
+ *
+ * Responsabilidade do chamador liberar a memória
+ * usando destruir_lista().
+ */
+
+Lista* criar_lista();
+
+/**
+ * Libera toda a memória associada a uma lista encadeada.
+ *
+ * Percorre a lista, liberando cada nó individualmente,
+ * e ao final libera a própria estrutura da lista.
+ *
+ * @param lista Ponteiro para a lista a ser destruída.
+ *              Se NULL, a função não realiza nenhuma ação.
+ *
+ * Após a chamada, o ponteiro passado torna-se inválido
+ * e não deve mais ser utilizado.
+ */
+void destruir_lista(Lista* lista);
+
+/**
+ * Insere um valor no início da lista encadeada
+ *
+ * @param lista Ponteiro para a lista.
+ * @param valor Valor a ser inserido.
+ * @return Status da inserção, 0 para erro e 1 para ok
+ *
+ * A lista deve ter sido criado previamente.
+ */
+int inserir_inicio(Lista* lista, int valor);
+
+/**
+ * Insere um valor no final da lista encadeada
+ *
+ * @param lista Ponteiro para a lista.
+ * @param valor Valor a ser inserido.
+ * @return Status da inserção, 0 para erro e 1 para ok
+ *
+ * Percorre a lista até o último nó para realizar a inserção.
+ */
+int inserir_fim(Lista* lista, int valor);
+
+/**
+ * Remove e retorna o valor do primeiro elemento da lista.
+ *
+ * @param lista Ponteiro para a lista.
+ *
+ * @return Valor removido, se ok == 1, se a lista estiver vazia ou
+ * NULL == 0.
+ */
+int remover_inicio(Lista* lista);
+
+/**
+ * Remove e retorna o valor do último elemento da lista.
+ *
+ * @param lista Ponteiro para a lista.
+ *
+ * @return Valor removido, se ok == 1, se a lista estiver vazia ou
+ * NULL == 0.
+ */
+int remover_fim(Lista* lista);
+
+/**
+ * Busca um valor na lista encadeada.
+ *
+ * @param lista Ponteiro para a lista.
+ * @param valor Valor a ser buscado.
+ *
+ * @return Ponteiro para o nó que contém o valor,
+ *         ou NULL caso não seja encontrado.
+ */
+ListaNo* buscar_valor(Lista* lista, int valor);
+
+/**
+ * Retorna a quantidade de elementos da lista.
+ *
+ * @param lista Ponteiro para a lista.
+ *
+ * @return Número de elementos armazenados na lista,
+ *         ou 0 se a lista for NULL.
+ */
+int tamanho_lista(Lista* lista);
+
+/**
+ * Verifica se a lista encadeada está vazia.
+ *
+ * @param lista Ponteiro para a lista.
+ *
+ * @return 1 se a lista estiver vazia,
+ *         0 caso contrário.
+ */
+int lista_vazia(Lista* lista);
+
+/**
+ * Gera uma representação da lista encadeada em formato JSON.
+ *
+ * O JSON contém os nós e suas ligações
+ *
+ * @param lista Ponteiro para a lista.
+ *
+ * @return String JSON alocada dinamicamente,
+ *         cuja liberação é responsabilidade do chamador.
+ */
+char* lista_para_json(Lista* lista, int modo_real);
+
+#endif
---
+++ core/src/lista.c
@@ -0,0 +1,137 @@
+#include "lista.h"
+#define LISTA_OK 1
+#define LISTA_ERRO 0
+
+Lista* criar_lista(){
+    // aloca espaço em memoria
+	Lista* lista = malloc(sizeof *lista);
+
+	// verifica se houve falha na alocação de memoria
+	if(lista == NULL)
+		return NULL;
+
+    // inicia a lista vazia
+	lista->inicio = NULL;
+    lista->tam    =    0;
+	// retorna o ponteiro alocado
+	return lista;
+}
+
+void destruir_lista(Lista* lista){
+    if(lista == NULL) return;
+
+    ListaNo* atual = lista->inicio;
+    ListaNo* prox;
+
+    //percorre até que a lista esteja vazia, e vai apagando os nós anteriores
+    while(atual != NULL){
+        prox = atual->prox;
+        free(atual);
+        atual = prox;
+    }
+    //apaga a lista
+    free(lista);
+}
+
+int inserir_inicio(Lista* lista, int valor){
+    if(lista == NULL) return LISTA_ERRO;
+
+    ListaNo* no = malloc(sizeof *no);
+    if(no == NULL) return LISTA_ERRO;
+    
+    no->valor = valor;
+    no->prox = lista->inicio;
+    lista->inicio = no;
+    lista->tam++;
+
+    return LISTA_OK;
+}
+
+int inserir_fim(Lista* lista, int valor){
+    if(lista == NULL) return LISTA_ERRO;
+    
+    ListaNo* no = malloc(sizeof *no);
+    if(no == NULL) return LISTA_ERRO;
+
+    no->valor = valor;
+    no->prox = NULL;
+
+    if (lista->inicio == NULL) {
+        // lista vazia
+        lista->inicio = no;
+    } else {
+        ListaNo* atual = lista->inicio;
+        while (atual->prox != NULL)
+            atual = atual->prox;
+        atual->prox = no;
+    }
+
+    lista->tam++;
+    return LISTA_OK;
+}
+
+int remover_inicio(Lista* lista){
+    if(lista == NULL) return LISTA_ERRO; // lista nao foi criada
+    if(lista->inicio == NULL) return LISTA_ERRO; // lista vazia, sem nada pra remover
+
+    ListaNo* atual = lista->inicio;
+    ListaNo* no = atual->prox;
+
+    lista->inicio = no;
+    free(atual);
+    lista->tam--;
+    return LISTA_OK;
+}
+
+int remover_fim(Lista* lista){
+    if(lista == NULL) return LISTA_ERRO; // lista nao foi criada
+    if(lista->inicio == NULL) return LISTA_ERRO; // lista vazia, sem nada pra remover
+
+    ListaNo* atual = lista->inicio;
+    ListaNo* anterior;
+
+    // caso com apenas 1 elemento
+    if(atual->prox == NULL){
+        free(atual);
+        lista->tam--;
+        lista->inicio = NULL;
+        return LISTA_OK;
+    }
+
+    // caso com mais de 1 elemento
+    while(atual->prox != NULL){
+        anterior = atual;
+        atual = anterior->prox;
+    }
+
+    anterior->prox = NULL;
+    free(atual);
+    lista->tam--;
+    return LISTA_OK;
+}
+
+ListaNo* buscar_valor(Lista* lista, int valor){
+    if(lista == NULL || lista->inicio == NULL) return NULL;
+
+    ListaNo* atual = lista->inicio;
+    while(atual != NULL){
+        if(atual->valor == valor) return atual;
+        atual = atual->prox;
+    }
+
+    return NULL;
+}
+
+int tamanho_lista(Lista* lista){
+    if(lista == NULL) return 0;
+    return lista->tam;
+}
+
+int lista_vazia(Lista* lista){
+    if(lista == NULL) return 1; // a lista nula é o mesmo que vazia
+    return lista->tam == 0 ? 1 : 0;
+}
+
+char* lista_para_json(Lista* lista){
+    
+}
